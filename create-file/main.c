aff3c5bbeab6cb3b74024ed83253cb678248463f
lucas4lves <zombnfl@gmail.com>
2024-05-21 03:15:00 -0300

    Very late nite resolution

--- create-file/main.c
+++ create-file/main.c
@@ -5,5 +5,14 @@
 int main(){
 	//TODO(LUCAS): Learn about the open() function!
 	//Here we're gonna write a program that's able to create a file
+	int file_descriptor;	
+
+	if((file_descriptor = open("dummy.txt", O_WRONLY | O_CREAT, S_IRWXU) < 0)){
+		printf("Failed to create or open such file \n");
+		return 1;
+	}
+
+	printf("File creation successful\n");
+	
 	return 0;	
 }
